2f02a1b3a9d9b8e14a2a155bdb4af12cfad65104
Simon Weis <laguna.ws@gmail.com>
2023-05-02 16:33:58 +0200

    Actually print message

--- impl/game_client_lib/client_network_connection.cpp
+++ impl/game_client_lib/client_network_connection.cpp
@@ -10,32 +10,28 @@
 #include <thread>
 
 ClientNetworkConnection::ClientNetworkConnection(
-    std::string const& ip, std::uint16_t serverPort, std::uint16_t clientPort)
-    : m_ip { ip }
-    , m_serverPort { serverPort }
-    , m_clientPort { clientPort }
-{
+        std::string const &ip, std::uint16_t serverPort, std::uint16_t clientPort)
+        : m_ip{ip}, m_serverPort{serverPort}, m_clientPort{clientPort} {
     // TODO load default from config file
     // TODO pass in logger and use logger instead of cout
     // TODO use separate thread so main thread is not blocked
 }
 
-void ClientNetworkConnection::establishConnection()
-{
+void ClientNetworkConnection::establishConnection() {
     asio::ip::udp::resolver resolver(m_IOContext);
     m_socket = std::make_unique<asio::ip::udp::socket>(m_IOContext,
-        asio::ip::udp::endpoint { NetworkProperties::NetworkProtocolType(), m_clientPort });
+                                                       asio::ip::udp::endpoint{NetworkProperties::NetworkProtocolType(),
+                                                                               m_clientPort});
 
     m_sendToEndpoint = *resolver
-                            .resolve(NetworkProperties::NetworkProtocolType(), m_ip,
-                                std::to_string(m_serverPort))
-                            .begin();
+            .resolve(NetworkProperties::NetworkProtocolType(), m_ip,
+                     std::to_string(m_serverPort))
+            .begin();
 
     startReceive();
 }
 
-void ClientNetworkConnection::sendPing()
-{
+void ClientNetworkConnection::sendPing() {
     if (!m_socket) {
         std::cerr << "socket not open\n";
         return;
@@ -44,22 +40,21 @@ void ClientNetworkConnection::sendPing()
     // send ping
     std::cout << "send ping to '" << m_sendToEndpoint.address() << " : " << m_sendToEndpoint.port()
               << "'\n";
-    std::array<char, 1> send_buf = { { 0 } };
+    std::array<char, 1> send_buf = {{0}};
     asio::error_code error;
     auto size = m_socket->send_to(asio::buffer(send_buf), m_sendToEndpoint, 0, error);
 
     m_socket->async_receive_from(asio::buffer(recv_buffer_), m_receivedFromEndpoint,
-        std::bind(&ClientNetworkConnection::handleReceive, this, std::placeholders::_1,
-            std::placeholders::_2));
+                                 std::bind(&ClientNetworkConnection::handleReceive, this, std::placeholders::_1,
+                                           std::placeholders::_2));
     std::cout << "ping sent '" << size << "'\n";
     std::cout << error.message() << std::endl;
 }
 
-void ClientNetworkConnection::startReceive()
-{
+void ClientNetworkConnection::startReceive() {
     std::cout << "start thread to process async tasks\n";
 
-    m_thread = std::thread { [this]() {
+    m_thread = std::thread{[this]() {
         while (true) {
             if (m_stopThread.load()) {
                 std::cerr << "thread stopped\n";
@@ -69,36 +64,32 @@ void ClientNetworkConnection::startReceive()
             m_IOContext.run();
             m_IOContext.restart();
         }
-    } };
+    }};
 }
 
 void ClientNetworkConnection::handleReceive(
-    const asio::error_code& /*error*/, std::size_t bytes_transferred)
-{
+        const asio::error_code & /*error*/, std::size_t bytes_transferred) {
     std::cout << "message received from '" << m_receivedFromEndpoint.address() << ":"
               << m_receivedFromEndpoint.port() << "'\n";
     // Note that recv_buffer might be a long buffer, but we only use the first "bytes transferred"
     // bytes from it.
-    std::string buf;
-    //    buf.resize(bytes_transferred);
-    //    m_receive_buffer.sgetn(&buf[0], buf.size());
-    //    std::cout.write(recv_buffer_.str(), bytes_transferred);
-    std::cout << buf << "\n\n";
+
+    std::cout.write(recv_buffer_.data(), bytes_transferred);
 }
 
 void ClientNetworkConnection::handle_send(std::shared_ptr<std::array<char, 1>> /*message*/,
-    const asio::error_code& /*error*/, std::size_t /*bytes_transferred*/)
-{
+                                          const asio::error_code & /*error*/, std::size_t /*bytes_transferred*/) {
 }
-void ClientNetworkConnection::disconnect() { }
-ClientNetworkConnection::~ClientNetworkConnection()
-{
+
+void ClientNetworkConnection::disconnect() {}
+
+ClientNetworkConnection::~ClientNetworkConnection() {
     disconnect();
     stopThread();
     m_socket.reset();
 }
-void ClientNetworkConnection::stopThread()
-{
+
+void ClientNetworkConnection::stopThread() {
     m_stopThread.store(true);
     m_thread.join();
 }
