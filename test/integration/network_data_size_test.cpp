d273c5b0f0e3c0b3a6729004518eae56cf417eac
Simon Weis <laguna.ws@gmail.com>
2023-07-09 09:25:53 +0200

    Use catch2 as testing framework for integration test

--- test/integration/network_data_size_test.cpp
+++ test/integration/network_data_size_test.cpp
@@ -1,3 +1,4 @@
+#include <catch2/catch.hpp>
 #include <compression/compressor_impl.hpp>
 #include <game_simulation.hpp>
 #include <log/logger_null.hpp>
@@ -8,11 +9,12 @@
 #include <receive_buffer.hpp>
 #include <simulation_result_sender_interface.hpp>
 #include <system_helper.hpp>
-#include <gtest/gtest.h>
+#include <iostream>
 
 class NetworkMessageCollector : public SimulationResultSenderInterface {
 public:
     std::string messageString { "" };
+
     void sendSimulationResults(SimulationResultDataForAllFrames const& data) override
     {
         nlohmann::json j = data;
@@ -20,14 +22,39 @@ public:
     }
 };
 
-TEST(NetworkDataSizeTest, NoUnitsSize)
+TEST_CASE("Size of game simulation network data", "[Integration, NetworkDataSizeTest]")
 {
     jt::null_objects::LoggerNull logger {};
-    UnitInfoCollection units { logger, std::vector<UnitInfo> {} };
+    UnitInfoCollection units { logger, "assets/units/all_units.json" };
     GameSimulation gs { logger, units };
-    gs.prepareSimulationForNewRound();
-
     NetworkMessageCollector collector;
+    SECTION("No units") { }
+    SECTION("100 Archers")
+    {
+        for (auto i = 0; i != 100; ++i) {
+            UnitClientToServerData data;
+            data.unitType = "archer";
+            data.playerID = jt::Random::getInt(0, 1);
+            data.unitID = i;
+            data.positionX = (i % terrainWidthInChunks) * terrainChunkSizeInPixel;
+            data.positionY = (i / terrainWidthInChunks) * terrainChunkSizeInPixel;
+            gs.addUnit(data);
+        }
+    }
+    SECTION("200 random units")
+    {
+        auto const types = units.getTypes();
+        for (auto i = 0; i != 200; ++i) {
+            UnitClientToServerData data;
+            data.unitType = *jt::SystemHelper::select_randomly(types.cbegin(), types.cend());
+            data.playerID = jt::Random::getInt(0, 1);
+            data.unitID = i;
+            data.positionX = (i % terrainWidthInChunks) * terrainChunkSizeInPixel;
+            data.positionY = (i / terrainWidthInChunks) * terrainChunkSizeInPixel;
+            gs.addUnit(data);
+        }
+    }
+    gs.prepareSimulationForNewRound();
     gs.performSimulation(collector);
 
     CompressorImpl compressor;
@@ -37,67 +64,5 @@ TEST(NetworkDataSizeTest, NoUnitsSize)
 
     // ensure the data fits into the receive buffer.
     ReceiveBuffer buffer;
-    EXPECT_LE(compressed.size(), buffer.data.size());
-    //    std::cout << collector.messageString << std::endl;
-}
-
-TEST(NetworkDataSizeTest, OneHundretArchers)
-{
-    jt::null_objects::LoggerNull logger {};
-    UnitInfoCollection units { logger, "assets/units/all_units.json" };
-    GameSimulation gs { logger, units };
-    for (auto i = 0; i != 100; ++i) {
-        UnitClientToServerData data;
-        data.unitType = "archer";
-        data.playerID = jt::Random::getInt(0, 1);
-        data.unitID = i;
-        data.positionX = (i % terrainWidthInChunks) * terrainChunkSizeInPixel;
-        data.positionY = (i / terrainWidthInChunks) * terrainChunkSizeInPixel;
-        gs.addUnit(data);
-    }
-
-    gs.prepareSimulationForNewRound();
-
-    NetworkMessageCollector collector;
-    gs.performSimulation(collector);
-
-    CompressorImpl compressor;
-    auto const compressed = compressor.compress(collector.messageString);
-    std::cout << "Network data size for simulation with 100 archers: " << compressed.size()
-              << std::endl;
-
-    // ensure the data fits into the receive buffer.
-    ReceiveBuffer buffer;
-    EXPECT_LE(compressed.size(), buffer.data.size());
-}
-
-TEST(NetworkDataSizeTest, TwoHundretRandomUnits)
-{
-    jt::null_objects::LoggerNull logger {};
-    UnitInfoCollection units { logger, "assets/units/all_units.json" };
-    GameSimulation gs { logger, units };
-    auto const types = units.getTypes();
-    for (auto i = 0; i != 200; ++i) {
-        UnitClientToServerData data;
-        data.unitType = *jt::SystemHelper::select_randomly(types.cbegin(), types.cend());
-        data.playerID = jt::Random::getInt(0, 1);
-        data.unitID = i;
-        data.positionX = (i % terrainWidthInChunks) * terrainChunkSizeInPixel;
-        data.positionY = (i / terrainWidthInChunks) * terrainChunkSizeInPixel;
-        gs.addUnit(data);
-    }
-
-    gs.prepareSimulationForNewRound();
-
-    NetworkMessageCollector collector;
-    gs.performSimulation(collector);
-
-    CompressorImpl compressor;
-    auto const compressed = compressor.compress(collector.messageString);
-    std::cout << "Network data size for simulation with 200 random units: " << compressed.size()
-              << std::endl;
-
-    // ensure the data fits into the receive buffer.
-    ReceiveBuffer buffer;
-    EXPECT_LE(compressed.size(), buffer.data.size());
+    REQUIRE(compressed.size() < buffer.data.size());
 }
