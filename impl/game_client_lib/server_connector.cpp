2f89af0fd0f8dc998a090bb5a0ce92110b5cbab0
Semitalis <kerim.merdenoglu@gmail.com>
2023-07-19 17:53:00 +0200

    remember IP input

--- impl/game_client_lib/server_connector.cpp
+++ impl/game_client_lib/server_connector.cpp
@@ -2,15 +2,41 @@
 #include <game_interface.hpp>
 #include <message.hpp>
 #include <state_game.hpp>
+#include <fstream>
 #include <imgui.h>
 #include <misc/cpp/imgui_stdlib.h>
+
+namespace {
+
+void saveIp(std::string const& ip)
+{
+    std::ofstream file { "ip.txt", std::ios_base::trunc };
+    if (file.is_open()) {
+        file << ip;
+    }
+}
+
+std::string loadIp()
+{
+    std::ifstream file { "ip.txt" };
+    std::string ip;
+    if (file.is_open()) {
+        file >> ip;
+    }
+    if (ip.empty()) {
+        return NetworkProperties::DefaultIPAddress();
+    }
+    return ip;
+}
+
+} // namespace
 
 ServerConnector::ServerConnector(std::shared_ptr<CompressorInterface> compressor)
     : m_compressor { compressor }
 {
 }
 
-void ServerConnector::doCreate() { }
+void ServerConnector::doCreate() { m_ip = loadIp(); }
 
 void ServerConnector::doUpdate(float const /*elapsed*/) { }
 
@@ -24,6 +50,7 @@ void ServerConnector::doDraw() const
     ImGui::Checkbox("Add bot as blue player (left)", &m_addBotAsPlayerZero);
     ImGui::Checkbox("Add bot as red player (right)", &m_addBotAsPlayerOne);
     if (ImGui::Button("connect")) {
+        saveIp(m_ip);
         m_connection = std::make_shared<ClientNetworkConnection>(
             m_ip, m_serverPort, m_clientPort, getGame()->logger(), m_compressor);
         m_connection->establishConnection();
